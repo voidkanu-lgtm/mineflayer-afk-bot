5a5cea4e4a3e3d8537909ac386995d127747ccb6
voidkanu-lgtm <voidkanu@gmail.com>
2026-05-28 17:33:13 +0530

    Set up basic Express server in .js

--- bot.js
+++ bot.js
@@ -1,3 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Bot is running')
+})
+
+app.listen(3000, () => {
+  console.log('Web server running!')
+})
 const mineflayer = require('mineflayer')
 
 const config = {
